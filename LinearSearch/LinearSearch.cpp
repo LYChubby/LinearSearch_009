c57079dcc22f8425ec6f0817f31d0d198a4d949c
LYChubby <f4uzi345@gmail.com>
2023-04-13 10:04:32 +0700

    Membuat Prosedur Input

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -11,6 +11,18 @@ void input() {
 
 		cout << "Enter The Number of Element In The Array : ";
 		cin >> n;
-		if
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nEelement Array Should Have Minimum 1 and Maximum 20 Elements.\n\n";
+	}
+	
+	cout << "\n--------------------\n";
+	cout << "Enter Array Elements \n";
+	cout << "-------------------- \n";
+
+	for (i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
 	}
 }
