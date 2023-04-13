747fc16dfd02c34949d24c34d3e5ae64e9c0ba08
LYChubby <f4uzi345@gmail.com>
2023-04-13 10:24:52 +0700

    Membuat Prosedur LinearSearch

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -26,3 +26,33 @@ void input() {
 		cin >> arr[i];
 	}
 }
+
+void LinearSearch() {
+	char ch;
+	int comparison;
+
+	do {																// Langkah 3
+
+		cout << "\nEnter The Element You Want To Search : ";			// Langkah 1
+		int item;
+		cin >> item;
+
+		comparison = 0;
+		for (i = 0; i < n; i++) {										// Langkah 2 & 4
+
+			comparison++;
+			if (arr[i] == item) {										// Langkah 5 Found
+
+				cout << "\n" << item << "Found at Position" << (i + 1) << endl;
+				break;
+			}
+		}
+
+		if (i == n)
+			cout << "\n" << item << "Not Found In The Array\n";
+		cout << "\nNumber Of Comparison : " << comparison << endl;
+
+		cout << "\nContinue Search (y/n) : ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
